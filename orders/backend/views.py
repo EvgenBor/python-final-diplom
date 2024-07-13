aad83660250c60d8e14ec075ee4058b9bad8b9bd
EvBor <evbor@mail.ru>
2024-07-14 00:31:03 +0500

    add_trottle

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -12,6 +12,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from ujson import loads as load_json
 from yaml import load as load_yaml, Loader
 from drf_spectacular.utils import extend_schema
